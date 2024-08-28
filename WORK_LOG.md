0037d00a8c558189d43967a6cdbde877f67e9f63
soccer47 <stevie.halprin@menloschool.org>
2024-08-28 15:49:10 -0700

    I finished coding, and solved the puzzle, and also just commented my code

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,14 @@
-Name: YOUR NAME HERE
+Name: Stevie K H
 
-| Date   |    Time    |                                                                    Update |
-|:-------|:----------:|--------------------------------------------------------------------------:|
-| Aug 22 |  8-8:30pm  | I started by thinking about my place in the universe.<br/>It was scary... |
-|        |            |                                                             And then I... |
+| Date   |         Time         |                                                                                                                                                                                        Update |
+|:-------|:--------------------:|----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Aug 22 | time in class-4:00pm |                                                                                                                    I figured out how how to decode 1-3, and have written the code to decode 1 |
+| Aug 22 |     4:00-4:30pm      |                                                                                    I coded the deciphering code for messages 2 and 3, and am currently trying to figure out how to decipher 4 |
+| Aug 22 |     4:30-5:00pm      |                                                                                                I coded problem number 4, however I'm not sure if the tester algorithm is functioning properly |
+| Aug 22 |  7-8pm, 9:15-9:30pm  |                                                 I recoded the functions 1-3 so that they functioned correctly, as I mistakelnly thought that they worked before. (forgot to update log sorry) |
+| Aug 23 |       8-8:30am       |                       I've done everything for messages 1-3, but still don't know how to decipher 4. I think it has something to do with UTF-8 or unicode, and I think the truck is the space |
+| Aug 23 |     5:20-5:56pm      | I figured out how to decipher message 4 (Unicode value of characters - 10,000 + 81, with 81 being equal to ascii val of lowercase 'a' - 19, which is unicode val of the truck/space - 10,000) |
+| Aug    |        5:58pm        |                                                                                                                                                                                      finished |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -9,34 +9,102 @@
 public class Riddler {
 
     public String decryptOne(String encrypted) {
+        // Empty string to be returned
         String decrypted = "";
+        // Traverses through each character in the encoded String
+        for (int i = 0; i < encrypted.length(); i++) {
+            int charVal = encrypted.charAt(i);
+            // If character isn't a letter, don't change its value
+            if (charVal <= 64 || (charVal >= 91 && charVal <= 96)) {
+                charVal += 0;
+            }
+            // If the character is upper case, perform caesar shift on letter value (+9)
+            else if (charVal <= 90) {
+                charVal = (charVal - 'A' + 9)%26 + 'A';
+            }
+            // If the character is lower case, perform caesar shift on letter value (+9)
+            else if (charVal <= 122){
+                charVal = (charVal - 'a' + 9)%26 + 'a';
+            }
+            // Add the ASCII value of the int to the message to be returned
+            decrypted += (char)charVal;
+        }
 
-        // TODO: Complete the decryptOne() function.
-
+        // Print the decoded message
+        System.out.print(decrypted);
+        // Return the decoded message
         return decrypted;
     }
 
     public String decryptTwo(String encrypted) {
+        // String to be returned starts as empty string
         String decrypted = "";
+        // String that will hold the characters that are individually added
+        String value = "";
+        // Traverses through each character of the encoded message
+        for (int i = 0; i < encrypted.length(); i++) {
+            // If the end of the number is reached, add the ascii value of the number to the return string
+            if (encrypted.substring(i, i + 1).equals(" ")) {
+                decrypted += (char)(Integer.parseInt(value));
+                // Reset the value of the string that holds the characters that are individually added
+                value = "";
+            }
+            else {
+                // Otherwise add the given character to the temporary String
+                value += encrypted.charAt(i);
+            }
+        }
 
-        // TODO: Complete the decryptTwo() function.
-
+        // Print the decoded message
+        System.out.print(decrypted);
+        // Return the decoded message
         return decrypted;
     }
 
     public String decryptThree(String encrypted) {
+        // String to be returned starts as empty string
         String decrypted = "";
+        // String that will hold added chars until length is 8
+        String binaryVal = "";
+        // Traverses through each character of the encoded message
+        for (int i = 0; i < encrypted.length(); i++) {
+            binaryVal += encrypted.charAt(i);
+            // If the length of value is 8, get the char from the value of the 8-bit int
+            if ((i + 1) % 8 == 0) {
+                int decVal = Integer.parseInt(binaryVal, 2);
+                // Add the resulting char to the String to be returned
+                decrypted += (char)decVal;
+                // Reset the String that holds the characters added individually
+                binaryVal = "";
+            }
 
-        // TODO: Complete the decryptThree() function.
+        }
 
+        // Print the decoded message
+        System.out.print(decrypted);
+        // Return the decoded message
         return decrypted;
     }
 
     public String decryptFour(String encrypted) {
+        // String to be returned starts as empty string
         String decrypted = "";
+        // Traverses through each character of the encoded message
+        for (int i = 0; i < encrypted.length(); i++) {
+            // If the character is a truck, add a space to the message
+            if (encrypted.charAt(i) == '⛟') {
+                decrypted += " ";
+            }
+            else {
+                // Otherwise, add the ascii value for the shifted value of the character in unicode to the return string
+                // *unicode value of the char - distance from start of dingbats in unicode to capital A in unicode
+                decrypted += Character.toString((char)(encrypted.codePointAt(i) - 9919));
+            }
+        }
 
-        // TODO: Complete the decryptFour() function.
-
+        // Print the decoded message
+        System.out.print(decrypted);
+        // Return the decoded message
         return decrypted;
     }
 }
